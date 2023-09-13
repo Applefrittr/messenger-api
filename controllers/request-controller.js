3c159ddd0cb8da13273f6c380b7de17ce95809d0
applefrittr <jacob.lee.jordan@gmail.com>
2023-09-13 16:41:03 -0400

    handle accept/decline controllers for requests

--- controllers/request-controller.js
+++ controllers/request-controller.js
@@ -33,39 +33,56 @@ exports.request_POST = asyncHandler(async (req, res, next) => {
 });
 
 exports.accept_request_POST = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ username: req.params.user }).exec();
-  const recipient = await User.findOne({
+  const user1 = await User.findOne({ username: req.params.user })
+    .populate("requestIn")
+    .exec();
+  const user2 = await User.findOne({
     username: req.params.recipient,
-  }).exec();
+  })
+    .populate("requestOut")
+    .exec();
 
-  user.requestOut = user.requestOut.filter(
-    (friend) => friend.username != recipient.username
+  user1.requestIn = user1.requestIn.filter(
+    (friend) => friend.username !== user2.username
   );
-  recipient.requestIn = recipient.requestIn.filter(
-    (friend) => friend.username != user.username
+  user2.requestOut = user2.requestOut.filter(
+    (friend) => friend.username !== user1.username
   );
 
-  user.friends.push(recipient);
-  recipient.friends.push(user);
+  user1.friends.push(user2);
+  user2.friends.push(user1);
 
-  await user.save();
-  await recipient.save();
+  await user1.save();
+  await user2.save();
 
   res.json({ message: "Accepted" });
 });
 
 exports.decline_request_POST = asyncHandler(async (req, res, next) => {
-  const user = await User.findOne({ username: req.params.user }).exec();
-  const recipient = await User.findOne({
+  const user1 = await User.findOne({ username: req.params.user })
+    .populate("requestIn")
+    .exec();
+  const user2 = await User.findOne({
     username: req.params.recipient,
-  }).exec();
+  })
+    .populate("requestOut")
+    .exec();
 
-  user.requestOut = user.requestOut.filter(
-    (friend) => friend.username != recipient.username
+  user1.requestIn = user1.requestIn.filter(
+    (friend) => friend.username !== user2.username
   );
-  recipient.requestIn = recipient.requestIn.filter(
-    (friend) => friend.username != user.username
+  user1.requestOut = user1.requestOut.filter(
+    (friend) => friend.username !== user2.username
   );
+  user2.requestOut = user2.requestOut.filter(
+    (friend) => friend.username !== user1.username
+  );
+  user2.requestIn = user2.requestIn.filter(
+    (friend) => friend.username !== user1.username
+  );
+
+  await user1.save();
+  await user2.save();
 
   res.json({ message: "Declined" });
 });
