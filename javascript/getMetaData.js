48e4e3d3d2a3091ea22b8c2c7ac306c7dd00dae3
applefrittr <jacob.lee.jordan@gmail.com>
2024-02-27 17:12:05 -0500

    installed axios, refactored getMetaData, reinstall linkifyjs linkify-react

--- javascript/getMetaData.js
+++ javascript/getMetaData.js
@@ -1,45 +1,20 @@
 const cheerio = require("cheerio");
-const https = require("https");
+const axios = require("axios");
+const fs = require("fs");
 
 const getMetaData = async (url) => {
-  const html = await getURL(url);
-  const $ = cheerio.load(html);
-  const $meta = $("meta[property]");
-  console.log("page loaded", url);
-  console.log($meta.attr());
+  const response = await axios.get(url);
 
-  $meta.each((i, elem) => {
-    console.log($(elem).attr());
+  fs.writeFile("./index.html", response.data, (err) => {
+    if (err) throw err;
+    console.log("file written!");
   });
-  //console.log($meta.attr("content"));
+  const $ = cheerio.load(response.data);
 
-  //   for (const tag of Object.entries($meta)) {
-  //     console.log(tag.attr("property"));
-  //     console.log(tag.attr("content"));
-  //   }
-};
+  const $metaData = $("meta[property]");
 
-const getURL = (url) => {
-  return new Promise((resolve, reject) => {
-    try {
-      https
-        .get(url, (res) => {
-          let content = "";
-
-          res.on("data", (chunk) => {
-            content += chunk;
-          });
-
-          res.on("end", () => {
-            resolve(content);
-          });
-        })
-        .on("error", (e) => {
-          console.error(e);
-        });
-    } catch (e) {
-      reject(console.log(e));
-    }
+  $metaData.each((i, elem) => {
+    console.log($(elem).attr());
   });
 };
 
